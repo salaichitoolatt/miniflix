98e7a642fd19a17dacaa1d716e6928b4d0511080
Salai Chit Oo Latt <salaichitoolatt@outlook.com>
2025-02-09 01:27:35 +0630

    [feat] - Add Database support
    1. create db server

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,27 +1,49 @@
 const express = require("express");
 const http = require("http");
+const mongodb = require("mongodb");
 
 const PORT = process.env.PORT;
 const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
 const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
+const DBHOST = process.env.DBHOST;
+const DBNAME = process.env.DBNAME;
 
-const app = express();
+async function main() {
+  const client = await mongodb.MongoClient.connect(DBHOST);
+  const db = client.db(DBNAME);
+  const videosCollection = db.collection("videos");
 
-app.get("/video", async (req, res) => {
-  const forwardRequest = http.request(
-    {
-      host: VIDEO_STORAGE_HOST,
-      port: VIDEO_STORAGE_PORT,
-      path: "/video?path=sample.mp4",
-      method: "GET",
-      headers: req.headers,
-    },
-    (forwardResponse) => {
-      res.writeHead(forwardResponse.statusCode, forwardResponse.headers);
-      forwardResponse.pipe(res);
+  const app = express();
+
+  app.get("/video", async (req, res) => {
+    const videoId = new mongodb.ObjectId(req.query.id);
+    const videoRecord = await videosCollection.findOne({ _id: videoId });
+
+    if (!videoRecord) {
+      res.sendStatus(404);
+      return;
     }
-  );
-  req.pipe(forwardRequest);
-});
 
-app.listen(PORT, () => console.log(`Service online.`));
+    const forwardRequest = http.request(
+      {
+        host: VIDEO_STORAGE_HOST,
+        port: VIDEO_STORAGE_PORT,
+        path: `/video?path=${videoRecord.videoPath}`,
+        method: "GET",
+        headers: req.headers,
+      },
+      (forwardResponse) => {
+        res.writeHead(forwardResponse.statusCode, forwardResponse.headers);
+        forwardResponse.pipe(res);
+      }
+    );
+    req.pipe(forwardRequest);
+  });
+
+  app.listen(PORT);
+}
+
+main().catch((err) => {
+  console.error("Microservice failed to start.");
+  console.error((err && err.stack) || err);
+});
