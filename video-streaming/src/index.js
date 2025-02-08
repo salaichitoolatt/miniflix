0b466bc314d684ffe98855b20cf3c469d424c9e2
Salai Chit Oo Latt <salaichitoolatt@outlook.com>
2025-02-08 21:23:56 +0630

    [feat] - integrat video-streaming and azure storage

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,30 +1,27 @@
 const express = require("express");
-const fs = require("fs");
+const http = require("http");
+
+const PORT = process.env.PORT;
+const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
+const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
 
 const app = express();
 
-if (!process.env.PORT) {
-  throw new Error(
-    "Please specify the port number for the HTTP server with the environment variable PORT."
-  );
-}
-
-const PORT = process.env.PORT || 3000;
-
 app.get("/video", async (req, res) => {
-  const videoPath = "./videos/sample.mp4";
-
-  const stats = await fs.promises.stat(videoPath);
-
-  res.writeHead(200, {
-    "content-length": stats.size,
-    "content-type": "video/mp4",
-  });
-  fs.createReadStream(videoPath).pipe(res);
+  const forwardRequest = http.request(
+    {
+      host: VIDEO_STORAGE_HOST,
+      port: VIDEO_STORAGE_PORT,
+      path: "/video?path=sample.mp4",
+      method: "GET",
+      headers: req.headers,
+    },
+    (forwardResponse) => {
+      res.writeHead(forwardResponse.statusCode, forwardResponse.headers);
+      forwardResponse.pipe(res);
+    }
+  );
+  req.pipe(forwardRequest);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.listen(PORT, () => console.log(`Video streaming service is online.`));
+app.listen(PORT, () => console.log(`Service online.`));
