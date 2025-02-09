10014d54d220b83ae42ab98a5102502d0a037079
Salai Chit Oo Latt <salaichitoolatt@outlook.com>
2025-02-09 15:11:09 +0630

    [feat] - add history service

---
+++ history/src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const PORT = process.env.PORT;
+
+async function main() {
+  const app = express();
+
+  app.listen(PORT, () => console.log("Microservice online."));
+}
+
+main().catch((err) => {
+  console.error("Microservice failed to start.");
+  console.error((err && err.stack) || err);
+});
